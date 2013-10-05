b5604667d1725a49490fd1d5634fcff9d3ae76e8
Mohamed Magdy <eng.mohamagdy@gmail.com>
2013-10-05 22:36:51 +0200

    #12 adding fetch method in the item endpoint

--- lib/theguardian/api/item.rb
+++ lib/theguardian/api/item.rb
@@ -1,14 +1,27 @@
 module Theguardian
 	module Api
 		class Item < RecursiveOpenStruct
+			def initialize(*params)
+				content = params.first["content"] ? params.first.merge(params.first["content"]["fields"] ) : params.first
+				item_params = params.first["fields"] || content
+
+				super(item_params, params.last)
+			end
+
 			def self.process(params)
 				connection = Theguardian::Connection.new(resource: params[:id])
 				self.new(connection.get(self.process_params(params)), recurse_over_arrays: true)
 			end
 
+			def fetch
+				self.class.process({ id: URI(self.apiUrl).path })
+			end
+
 			def self.process_params(params)
+				# The item id
 				theguardian_api_params = { id: params[:id] }
 
+				# The show fields
 				theguardian_api_params.merge!({ "show-fields" => params[:fields] || "all" }) 
 
 				theguardian_api_params.reject{ |_, value| value.nil? }
