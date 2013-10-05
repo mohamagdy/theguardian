11785fdfc6269f6b9be01231b229850900668b92
Mohamed Magdy <eng.mohamagdy@gmail.com>
2013-10-05 23:45:17 +0200

    #12 connecting the section with the item

--- lib/theguardian/api/section.rb
+++ lib/theguardian/api/section.rb
@@ -2,8 +2,26 @@ module Theguardian
 	module Api
 		class Section < RecursiveOpenStruct
 			def self.process(params = {})
-				connection = Theguardian::Connection.new(resource: params.delete(:id) || "sections")
-				self.new(connection.get, recurse_over_arrays: true)
+				connection = Theguardian::Connection.new(resource: params[:id] || "sections")
+				response = connection.get
+
+				if params[:id]
+					self.new(response, recurse_over_arrays: true)
+				else
+					response["results"].map do |section_params|
+						self.new(section_params, recurse_over_arrays: true)
+					end
+				end
+			end
+
+			def items
+				self.results.map do |item_params|
+					Theguardian::Api::Item.new(item_params, recurse_over_arrays: true)
+				end
+			end
+
+			def fetch
+				self.class.process(id: self.id)
 			end
 
 			def self.process_params(params)
